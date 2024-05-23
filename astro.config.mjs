7850bab0a39341df810b0651033700787fdff215
JoseValero12 <joserck99@gmail.com>
2024-05-23 12:49:45 -0600

    conf2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "/",
+  site: "https://telco-web.vercel.app",
   integrations: [tailwind(), mdx(), sitemap()],
 });
