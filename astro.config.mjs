332df9f7e318ed5a07306e7244dcbb3897829c9a
JoseValero12 <joserck99@gmail.com>
2024-05-23 12:45:37 -0600

    conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "/",
   integrations: [tailwind(), mdx(), sitemap()],
 });
