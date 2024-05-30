ef79f93d28983cf8116196365777bd563ff850c2
yixxuz18 <yisuskroom@gmail.com>
2024-05-30 09:18:23 -0600

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://telco-networks.mx",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(),sitemap()],
 });
